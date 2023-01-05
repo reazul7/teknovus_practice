83367479f2d338fcd356ea8d4bf01a9f6bdb2f39
reazul7 <islamreazul7777@gmail.com>
2023-01-05 15:51:41 +0600

    value store using local storage

--- src/components/ReactCrud/ContactContext.js
+++ src/components/ReactCrud/ContactContext.js
@@ -1,32 +1,32 @@
-import React, { createContext, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid';
+import React, { createContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-export const CreateContactContext = createContext()
+export const CreateContactContext = createContext();
 
 function ContactContext(props) {
-    const [contacts, setContacts] = useState([
-        { id: uuidv4(), name: "adkuhcdskn", email: "@hdckjsd", phone: 73637 },
-        { id: uuidv4(), name: "zdfdvfd", email: "@XVDF", phone: 34334 },
-        { id: uuidv4(), name: "dfd", email: "@hdckjsd", phone: 4545 },
-      ]);
+    const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem("contacts")));
 
-      const addContact = (name, email, phone) => {
-        setContacts([...contacts, {id: uuidv4(), name, email, phone}])
-      }
+    useEffect(() => {
+        localStorage.setItem("contacts", JSON.stringify(contacts));
+    });
 
-      const deleteContact = (id) => {
-        setContacts(contacts.filter((contact) => contact.id !== id))
-      }
+    const addContact = (name, email, phone) => {
+        setContacts([...contacts, { id: uuidv4(), name, email, phone }]);
+    };
 
-      const editContact = (id, editedContact) => {
-        setContacts(contacts.map((contact) => contact.id === id ? editedContact : contact))
-      }
+    const deleteContact = (id) => {
+        setContacts(contacts.filter((contact) => contact.id !== id));
+    };
 
-  return (
-    <CreateContactContext.Provider value={{contacts, addContact, deleteContact, editContact}}>
-        {props.children}
-    </CreateContactContext.Provider>
-  )
+    const editContact = (id, editedContact) => {
+        setContacts(contacts.map((contact) => (contact.id === id ? editedContact : contact)));
+    };
+
+    return (
+        <CreateContactContext.Provider value={{ contacts, addContact, deleteContact, editContact }}>
+            {props.children}
+        </CreateContactContext.Provider>
+    );
 }
 
-export default ContactContext
+export default ContactContext;
